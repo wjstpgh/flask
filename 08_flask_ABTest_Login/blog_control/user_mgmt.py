05eedba69d0e4455afbb6130ad6b0e57c898b729
wjstpgh <sehoJ3756@gmail.com>
2022-07-15 02:19:58 +0900

    login

--- 08_flask_ABTest_Login/blog_control/user_mgmt.py
+++ 08_flask_ABTest_Login/blog_control/user_mgmt.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
-from blog_model.mysql import conn_mysqldb
+from db_model.mysql import conn_mysqldb
+
 
 class User(UserMixin):
 
@@ -21,25 +22,24 @@ class User(UserMixin):
         user = db_cursor.fetchone()
         if not user:
             return None
-        
+
         user = User(user_id=user[0], user_email=user[1], blog_id=user[2])
         return user
-
 
     @staticmethod
     def find(user_email):
         mysql_db = conn_mysqldb()
         db_cursor = mysql_db.cursor()
-        sql = "SELECT * FROM user_info WHERE USER_EMAIL = '" + str(user_email) + "'"
+        sql = "SELECT * FROM user_info WHERE USER_EMAIL = '" + \
+            str(user_email) + "'"
         # print (sql)
         db_cursor.execute(sql)
         user = db_cursor.fetchone()
         if not user:
             return None
-        
+
         user = User(user_id=user[0], user_email=user[1], blog_id=user[2])
         return user
-
 
     @staticmethod
     def create(user_email, blog_id):
@@ -47,9 +47,19 @@ class User(UserMixin):
         if user == None:
             mysql_db = conn_mysqldb()
             db_cursor = mysql_db.cursor()
-            sql = "INSERT INTO user_info (USER_EMAIL, BLOG_ID) VALUES ('%s', '%s')" % (str(user_email), str(blog_id))
+            sql = "INSERT INTO user_info (USER_EMAIL, BLOG_ID) VALUES ('%s', '%s')" % (
+                str(user_email), str(blog_id))
             db_cursor.execute(sql)
             mysql_db.commit()
             return User.find(user_email)
         else:
             return user
+
+    @staticmethod
+    def delete(user_id):
+        mysql_db = conn_mysqldb()
+        db_cursor = mysql_db.cursor()
+        sql = "DELETE FROM user_info WHERE USER_ID = %d" % (user_id)
+        deleted = db_cursor.execute(sql)
+        mysql_db.commit()
+        return deleted
